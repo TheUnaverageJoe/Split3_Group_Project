09f1de2cdb36243bf259468e4cd51cb4cbad417e
ericshen9999 <dragoneshen@gmail.com>
2021-11-22 19:37:04 -0800

    Edited Movement

--- Assets/Scripts/BasicMove.cs
+++ Assets/Scripts/BasicMove.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class BasicMove : MonoBehaviour
 {
     Rigidbody2D rb;
-    int jumpStrength = 10;
-    bool canJump = false;
-    bool canMove = false;
-    int moveSpeed = 5;
+    public int jumpStrength = 20;
+    public bool canJump = false;
+    public int moveSpeed = 5;
+    public int gravity = 5;
+    public int maxspeed = 10;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.drag = 5;
     }
 
     // Update is called once per frame
@@ -22,24 +25,28 @@ public class BasicMove : MonoBehaviour
         if(Input.GetButtonDown("Jump") && canJump){
             rb.velocity += Vector2.up*jumpStrength;
             canJump = false;
-            canMove = false;
         }
 
-        if(canMove){
-            if(Input.GetAxis("Horizontal") < 0){
-                rb.velocity = Vector2.zero;
-                rb.velocity += Vector2.left*moveSpeed;
-            }else if(Input.GetAxis("Horizontal") > 0){
-                rb.velocity = Vector2.zero;
-                rb.velocity += Vector2.right*moveSpeed;
-            }else{
-                rb.velocity = new Vector2(0, rb.velocity.y);
+        if(Input.GetAxis("Horizontal") < 0){
+            //rb.velocity += Vector2.down*gravity;
+            rb.velocity += Vector2.left*moveSpeed;
+            if(Math.Abs(rb.velocity.x) > maxspeed){
+                rb.velocity = new Vector2(-maxspeed,rb.velocity.y);
             }
+        }else if(Input.GetAxis("Horizontal") > 0){
+            //rb.velocity += Vector2.down*gravity;
+            rb.velocity += Vector2.right*moveSpeed;
+            if(Math.Abs(rb.velocity.x) > maxspeed){
+                rb.velocity = new Vector2(maxspeed,rb.velocity.y);
+            }
+        }
+
+        if(rb.velocity.y < -maxspeed){
+            rb.velocity = new Vector2(rb.velocity.x,-maxspeed);
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         canJump = true;
-        canMove = true;
     }
 }
