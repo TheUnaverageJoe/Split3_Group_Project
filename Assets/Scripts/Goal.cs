82ca80eee6ea1ad4ef3392ec130b90c2fc932e4a
rubencal99 <60902416+rubencal99@users.noreply.github.com>
2021-11-30 13:55:29 -0800

    Added Goal script
    There was a bug with two audio listeners, so I just turned off the end game screen listener bc there's no sound anyways.

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,15 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Victory!");
-    }
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        Debug.Log("Exited");
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("Scenes/EndGame Screen", LoadSceneMode.Additive);
     }
 }
